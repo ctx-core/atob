ee33fd92750f60885fc3303723bb3f73b73c5dd2
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/atob.js
+++
@@ -1,5 +0,0 @@
-import { atob_ } from './atob_';
-export function atob(val) {
-    return atob_()(val);
-}
-//# sourceMappingURL=src/atob.js.map
--- src/atob_.js
+++
@@ -1,8 +0,0 @@
-import { no_dom } from '@ctx-core/dom';
-export function atob_() {
-    return (typeof no_dom
-        ? require('atob-lite')
-        : window.atob);
-}
-export { atob_ as _atob, };
-//# sourceMappingURL=src/atob_.js.map
