66b2393f20560b210c1f445cfc00a9dea54e4052
Brian Takita <brian.takita@gmail.com>
2023-04-04 17:44:03 -0400

    formatting

--- src/atob_/index.browser.d.ts
+++ src/atob_/index.browser.d.ts
@@ -1,2 +1,4 @@
-export declare function atob_():((data:string)=>string)&typeof globalThis.atob;
+export declare function atob_():
+	((data:string)=>string)
+	&typeof globalThis.atob
 export { atob_ as _atob, }
